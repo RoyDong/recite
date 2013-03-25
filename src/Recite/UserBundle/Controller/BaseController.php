4c38eeff0a2120be61466519204dd3b177e635dd
Roy Dong <roy.dongwei@gmail.com>
2013-03-25 18:37:49 +0800

    tb

--- src/Recite/UserBundle/Controller/BaseController.php
+++ src/Recite/UserBundle/Controller/BaseController.php
@@ -3,14 +3,17 @@
 namespace Recite\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\EntityManager;
 
-/**
- * @Route("/security")
- */
 class BaseController extends Controller
 {
-
-    protected function __get($name){
+    /**
+     * 
+     * @param string $name
+     * @return Doctrine\ORM\EntityRepository;
+     */
+    public function __get($name){
         return $this->get('doctrine')->getRepository('ReciteDataBundle:'.$name);
     }
 
@@ -18,19 +21,23 @@ class BaseController extends Controller
      * get doctrine manager
      * 
      * @param string $name
-     * @return Manager
+     * @return EntityManager;
      */
-    protected function dm($name){
+    protected function em($name = null){
         return $this->get('doctrine')->getManager($name);
     }
 
-    protected function renderJson($data){
+    protected function renderJson($data = null){
         if (is_array($data) && 0 === count($data)) {
             $data = new \ArrayObject();
         }
 
-        // Encode <, >, ', &, and " for RFC4627-compliant JSON, which may also be embedded into HTML.
-        $json = json_encode($data, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP | JSON_HEX_QUOT | JSON_UNESCAPED_UNICODE );
+        // Encode  ', &, and " for RFC4627-compliant JSON, which may also be embedded into HTML.
+        if($data){
+            $json = json_encode($data, JSON_UNESCAPED_UNICODE );
+        }else{
+            $json = '';
+        }
 
         $response = new Response($json);
         $response->headers->set('Content-Type', 'application/json');
--- src/Recite/UserBundle/Controller/RegistrationController.php
+++ src/Recite/UserBundle/Controller/RegistrationController.php
@@ -4,6 +4,7 @@ namespace Recite\UserBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Recite\DataBundle\Entity\User;
 
 /**
  * @Route("/registration")
@@ -12,13 +13,30 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class RegistrationController extends BaseController
 {
     /**
-     * @Route("/account")
-     * @Method({"post"})
+     * @Route("/register")
      */
-    public function accountAction()
+    public function registerAction()
     {
-        $user = $this->getUser();
+        $request = $this->get('request');
+        $email = $request->get('email');
+        $user = $this->User->findOneByEmail($request->get('email'));
 
-        return ['name' => $user ? $user->getUsername() : 'annoymous'];
+        if($user){
+            return $this->renderJson(['error' => 'Email is used']);
+        }
+
+        $user = (new User)->setEmail($email);
+        $encoder = $this->get('security.encoder_factory')->getEncoder($user);
+        $user->setPassword($encoder->encodePassword($request->get('password'), $user->getSalt()));
+
+        return $this->renderJson(['name' => '董炜<>"&\'']);
+    }
+
+    private function login(){
+
+    }
+
+    private function rememberMe(){
+
     }
 }
